aae5f31833b09902ca2a389e15a6517caedd119e
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-01-28 15:17:11 -0500

    Created CourseTaken.kt

--- app/src/main/java/com/example/bcs421_homework1_mazen/CourseTaken.kt
+++ app/src/main/java/com/example/bcs421_homework1_mazen/CourseTaken.kt
@@ -1,4 +1,12 @@
 package com.example.bcs421_homework1_mazen
 
-class CourseTaken {
+class CourseTaken(
+    var courseName: String,
+    var startTime: String,
+    var endTime: String,
+    var days: String,
+    var credits: Int
+) {
+    //Medthods
+
 }
