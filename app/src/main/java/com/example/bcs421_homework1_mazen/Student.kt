25f5d3bd8d2720159dbd3aa5202faadd381341c9
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-01-30 15:55:44 -0500

    Created CourseTaken.kt

--- app/src/main/java/com/example/bcs421_homework1_mazen/Student.kt
+++ app/src/main/java/com/example/bcs421_homework1_mazen/Student.kt
@@ -1,4 +1,13 @@
 package com.example.bcs421_homework1_mazen
 
-class Student {
+class Student (
+    var coursesTaken: MutableList<CourseTaken> = mutableListOf()
+){
+    fun totalCredits(): Int {
+        var totalCredits = 0
+        for (course in coursesTaken) {
+            totalCredits += course.credits
+        }
+        return totalCredits
+    }
 }
